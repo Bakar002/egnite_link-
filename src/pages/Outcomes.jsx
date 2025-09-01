f6f9fc634d3085ca30fef2386919cb4aa709c90d
Ashar <ashar@example.com>
2025-09-01 17:51:41 +0500

    Asahr updated

--- src/pages/Outcomes.jsx
+++ src/pages/Outcomes.jsx
@@ -1,10 +1,13 @@
 import Header from "../components/Header";
 import MainBanner from "../components/MainBanner";
-import mainbg from "../assets/images/mainbg.png";
+import mainbg from "../assets/images/a08e9b4485628a3877efe2700adc74c98cf02ffe.png";
 import BusinessTransformation from './../components/Before';
 import ROITimeline from "../components/Time line";
 import Needsystem from "../components/Need system";
 import KeyOutcomeThemes from "../components/Outcome";
+import SixDivisionsComponent from "../components/SixDevisions";
+import StrategicUseCases from "../components/Strategic";
+import ProfessionalFooter from "../components/HomeFooter";
 
 
 function Outcomes(){
@@ -13,14 +16,19 @@ function Outcomes(){
         <>
         <Header />
       <MainBanner
-        heading={<>We Don’t Automate Businesses <br /> We Engineer Self-Sufficient <br /> Growth Architectures</>}
-        subHeading="ProIntellect Systems™ equips your business with certified-grade AI professionals — designed to support legal, financial, operational, medical, and executive decisions with structure, speed, and stability."
-        buttonText="Request ProIntellect Consultation"
+        heading={<>We Don’t Sell Software. <br /> We Build Systems That <br /> Produce Outcomes. <br /> Growth Architectures</>}
+        subHeading="E-Gnite is a business transformation infrastructure — where leads increase, fulfillment accelerates, bandwidth expands, and operations become self-governing. We measure our value in outcomes, not outputs."
+        buttonText="Request Private Session"
         backgroundImage={mainbg}
     />
+     <SixDivisionsComponent />
+           <StrategicUseCases />
+
+    
         <BusinessTransformation />
         <ROITimeline />
         <Needsystem />
+        <ProfessionalFooter />
         </>
     )
 }
